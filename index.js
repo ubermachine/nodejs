4b151bc58f2a4f70b24d471632c47e3d4f8bd3ab
ubermachine <wakinglife9@gmail.com>
2020-02-21 17:52:59 +0000

    added basic structure

---
+++ index.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = (q.pathname !== "/") ? "." + q.pathname + ".html" : "./index.html";
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            var errorFile = "./404.html";
+            fs.readFile(errorFile, function(err, data) {
+                if(err){
+                    res.writeHead(404, {'Content-Type': 'text/html'});
+                    return res.end("404 Not Found");
+                }
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                return res.end();
+            });
+        }
+        else{
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end();
+        }
+    });
+}).listen(8080); 
